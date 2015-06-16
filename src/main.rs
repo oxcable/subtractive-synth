21a3b177805f5006ce9cf602c7ac0c375ba7d5d5
Michael Nye <thenyeguy@gmail.com>
2015-06-16 17:37:09 -0400

    Update backend, attenuate synth output

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ mod subtractive_synth;
 
 fn main() {
     use oxcable::chain::DeviceChain;
+    use oxcable::dynamics::Limiter;
     use oxcable::io::audio::AudioEngine;
     use oxcable::io::midi::MidiEngine;
+    use oxcable::mixers::Gain;
     use oxcable::utils::tick::tick_until_enter;
 
     use subtractive_synth::SubtractiveSynth;
@@ -17,6 +19,10 @@ fn main() {
     let midi_engine = MidiEngine::open().unwrap();
     let mut chain = DeviceChain::from(
         SubtractiveSynth::new(midi_engine.choose_input(), 2)
+    ).into(
+        Gain::new(-6.0, 1)
+    ).into(
+        Limiter::new(-3.0, 0.0, 1)
     ).into(
         audio_engine.default_output(1)
     );
